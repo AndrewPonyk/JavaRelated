52fd277891da8f078152c3d0852a18f47ffae21f
AndrewPonyk <andrii.ponyk@globallogic.com>
2013-10-23 16:03:13 +0300

    Play1 added application to send email . Not finished git status!

---
+++ JavaEE/4WebFrameworks/Play/Play1/OficialDocumentation/14_Sending_Email/email/app/controllers/Application.java
@@ -0,0 +1,54 @@
+package controllers;
+
+import play.*;
+import play.libs.Mail;
+import play.mvc.*;
+
+import java.util.*;
+
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.HtmlEmail;
+import org.apache.commons.mail.SimpleEmail;
+
+import models.*;
+
+public class Application extends Controller {
+
+    public static void index() {
+        render();
+    }
+    
+    public static void sendMail(String to,String message){
+    	System.out.println(to);
+    	System.out.println(message);
+    	
+    	
+    	// there is also SimpleEmail class , but if you want to send html content we will use HtmlEmail
+    	HtmlEmail email=new HtmlEmail();
+    	try {
+			email.setFrom("andrew.ponuk9999@gmail.com");
+			email.addTo(to);
+			email.setSubject("subject");
+			
+			
+			
+			email.setHtmlMsg("<html><h3>Message header</h3>  <div>"+message+"</div></html>");
+			
+			email.setTextMsg(message); // simple plain
+			
+			
+			// set the alternative message
+			//email.setMsg("Your email client does not support HTML, too bad :(");
+			
+			
+			Mail.send(email); 
+			
+		} catch (EmailException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	renderText("OK");
+    }
+
+}
