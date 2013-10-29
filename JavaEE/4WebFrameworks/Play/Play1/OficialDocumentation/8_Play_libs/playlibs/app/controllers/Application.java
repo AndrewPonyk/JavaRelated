370a6d515c1964e3653c22551cbda70ee3ad7bd2
AndrewPonyk <andrew9999@ukr.net>
2013-10-29 08:32:03 +0200

    8_Play libs started

--- JavaEE/4WebFrameworks/Play/Play1/OficialDocumentation/8_Play_libs/playlibs/app/controllers/Application.java
+++ JavaEE/4WebFrameworks/Play/Play1/OficialDocumentation/8_Play_libs/playlibs/app/controllers/Application.java
@@ -1,17 +1,63 @@
 package controllers;
 
 import play.*;
+import play.libs.WS;
+import play.libs.WS.HttpResponse;
+import play.libs.XPath;
 import play.mvc.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
 
 import models.*;
 
 public class Application extends Controller {
 
+	
+	
+	public static void readXMLWithXPath() throws SAXException, IOException, ParserConfigurationException{
+		
+	File fXmlFile = new File(Play.applicationPath.toString()+"/"+"doc.xml");
+	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	Document doc = dBuilder.parse(fXmlFile);
+		
+	for(Node event: XPath.selectNodes("events//event", doc)) {
+		  String id = XPath.selectText("@id", event);
+		
+		  System.out.println(id);
+	}
+
+		renderArgs.put("ApplicationPath", Play.applicationPath.toString());
+		render();
+	}
+	
+	
+	
+	public static void WebServiceClient(){
+		
+		HttpResponse res = WS.url("http://www.google.com").get();
+		
+		
+		renderArgs.put("googleContent", res.getString());
+		renderArgs.put("ApplicationPath", Play.applicationPath.toString());
+		render();
+	}
+	
+	
+	
     public static void index() {
     	
-    	
-        render();
+    	renderArgs.put("ApplicationPath", Play.applicationPath.toString());
+    	render();
     }
 }
