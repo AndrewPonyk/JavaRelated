fa555fb35403804e849b7649d58709f12102aa08
AndrewPonyk <andrew9999@ukr.net>
2013-11-01 09:12:20 +0200

    Play1 : Official doc : 11 Ajax in play not finished yet

--- JavaEE/4WebFrameworks/Play/Play1/OficialDocumentation/11_Ajax_in_Play1/ajaxinplay1/app/controllers/Application.java
+++ JavaEE/4WebFrameworks/Play/Play1/OficialDocumentation/11_Ajax_in_Play1/ajaxinplay1/app/controllers/Application.java
@@ -9,8 +9,32 @@ import models.*;
 
 public class Application extends Controller {
 
-    public static void index() {
+    
+	public static void index() {
         render();
     }
+	
+	
+	public static void devices(Integer id){
+		
+		//create 'Model' =)
+		List<String>  devices=new ArrayList<>();
+		devices.add("1,Sony");
+		devices.add("2,HTC");
+		devices.add("3,Samsung");
+		
+		//get data
+		HashMap<String, String> result=new HashMap<>();
+		result.put("id", devices.get(id).split(",")[0]);
+		result.put("name", devices.get(id).split(",")[1]);
+		
+		
 
+		renderTemplate("Application/devices.xml",result);
+	}
+    
 }
+
+
+
+
