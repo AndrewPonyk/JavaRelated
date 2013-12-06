25bc2d8fa3e11ffe7b1d7f0f7ca14e884963e267
AndrewPonyk <andrii.ponyk@globallogic.com>
2013-12-06 13:29:05 +0200

    Play 1 : Official doc : yabe continue , add posts views

--- JavaEE/4WebFrameworks/Play/Play1/OficialDocumentation/yabe_blog/yabe/app/controllers/Application.java
+++ JavaEE/4WebFrameworks/Play/Play1/OficialDocumentation/yabe_blog/yabe/app/controllers/Application.java
@@ -9,14 +9,31 @@ import models.*;
 
 public class Application extends Controller {
 
+	@Before
+	static void addDefaults() {
+	    renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+	    renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+	}
+	
     public static void index() {
-    	
-    	User bob =User.findById(1L);
+    	//test data
+    	/*
+    	 * User bob =User.findById(1L);
     	System.out.println(bob.posts.size());
     	for(Post item : bob.posts){
     		System.out.println(item.content);
-    	}
-        render();
+    	}*/
+    	
+    	  Post frontPost = Post.find("order by postedAt desc").first();
+          List<Post> olderPosts = Post.find(
+              "order by postedAt desc"
+          ).from(1).fetch(10);
+          render(frontPost, olderPosts);
     }
-
+    
+    public static void show(Long id){
+    	Post post=Post.findById(id);
+    	render(post);
+    }
+    
 }
