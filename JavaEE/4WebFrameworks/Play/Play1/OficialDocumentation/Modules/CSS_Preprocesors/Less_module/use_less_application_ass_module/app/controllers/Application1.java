0de448eb24de78f0bbbaaa77cf1cc796c3c1b8ea
AndrewPonyk <andrew9999@ukr.net>
2013-12-12 09:00:23 +0200

    Play1 : Modules : less module

---
+++ JavaEE/4WebFrameworks/Play/Play1/OficialDocumentation/Modules/CSS_Preprocesors/Less_module/use_less_application_ass_module/app/controllers/Application1.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+public class Application1 extends Controller {
+
+    public static void index() {
+    	play.cache.Cache.clear();
+        render();
+    }
+
+}
