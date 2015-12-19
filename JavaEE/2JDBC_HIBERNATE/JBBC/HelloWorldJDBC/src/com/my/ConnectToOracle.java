6a6a044dff353c33e1d3bac350b7576cfd0ccff2
AndrewPonyk <andrew9999@ukr.net>
2015-12-20 01:17:54 +0200

    Added hello jdbc world, update gwt project to eclipse on new nout

---
+++ JavaEE/2JDBC_HIBERNATE/JBBC/HelloWorldJDBC/src/com/my/ConnectToOracle.java
@@ -0,0 +1,27 @@
+package com.my;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ConnectToOracle {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		System.out.println("Connecting to oracle db");
+		
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:22337:orcl",
+				"system", "Aa123456");
+		
+		Statement statement = conn.createStatement();
+		ResultSet rs = statement.executeQuery("select * from users");
+		
+		while(rs.next()){
+			System.out.println(String.format("%s _ %s", rs.getString("ID"), rs.getString("NAME")));
+		}
+		
+		conn.close();
+		
+	}
+}
