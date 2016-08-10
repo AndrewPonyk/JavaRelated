6d4cac1ef90393e9eb1333cfd20036b42203e185
Andrii Ponyk <APonyk@agileassets.com>
2016-08-10 17:31:05 +0300

    Added project : java work with archives

--- JavaEE/22OtherUsefulApisAndLibraries/WorkWithDocAndExcel/ApachePoiExamples/src/main/java/com.ap/ModifyWordDoc.java
+++ JavaEE/22OtherUsefulApisAndLibraries/WorkWithDocAndExcel/ApachePoiExamples/src/main/java/com.ap/ModifyWordDoc.java
@@ -1,45 +1,83 @@
 package com.ap;
 
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 import org.apache.commons.lang3.text.StrSubstitutor;
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.*;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.IntStream;
 
-/**
- * Created by aponyk on 05.08.2016.
- */
 public class ModifyWordDoc {
+
     public static void main(String[] args) throws IOException {
-        System.err.println("Modify word document");
-        XWPFDocument doc = new XWPFDocument(new FileInputStream("/home/andrii/temp/ApachePoiExamples/letter-template.docx"));
-        Map<String, String> values = new HashMap<>();
-        values.put("S2_R1_FName", "Roman");
-        values.put("S2_R1_MI", "Romanenko2");
-        values.put("S2_R1_LName", "Romanenko4");
-        values.put("agreement.amount", "1000 $");
-        values.put("agreement.amount.text", "One thousand dolars");
+        System.err.println("Modify word document : replace ${placeholders} by real some values");
 
-
+        XWPFDocument doc = new XWPFDocument(new FileInputStream("D:\\temp\\letter-template.docx"));
+        // substitute placeholders in text
         StrSubstitutor sub = new StrSubstitutor(values, "${", "}");
-
         doc.getParagraphs().forEach(p -> {
+            p.getRuns().forEach(run -> {
+                String text = run.getText(0);
+                if (text != null) {
+                    System.out.println(text);
+                    String result = sub.replace(text);
+                    run.setText(result, 0);
+                }
+            });
+        });
 
-            p.getRuns().forEach(r -> {
-                Optional.of(r).ifPresent(run -> {
-                    String text = run.getText(0);
-                    if (text != null) {
-                        String result = sub.replace(text);
-                        run.setText(result, 0);
-                    }
+        final List<String> placeholders = new ArrayList<String>();
+        doc.getTables().forEach(tbl -> {
+            tbl.getRow(1).getTableCells().forEach(cell -> {
+                placeholders.add(cell.getText());
+            });
+            tbl.removeRow(1);
+
+            IntStream.range(0, table1Source.size()).forEach(i -> {
+                final XWPFTableRow row = tbl.createRow();
+                final StrSubstitutor substitutor = new StrSubstitutor(table1Source.get(i), "${", "}");
+                IntStream.range(0, placeholders.size()).forEach(cellIndex -> {
+                    row.getCell(cellIndex).setText(substitutor.replace(placeholders.get(cellIndex)));
                 });
             });
         });
-        doc.write(new FileOutputStream("/home/andrii/temp/ApachePoiExamples/TestDocWithPlaceholders_REPLACED.docx"));
+
+        doc.write(new FileOutputStream("D:\\temp\\letter-template_REPLACED.docx"));
         doc.close();
+    }
+
+    public static Map<String, String> values = new HashMap<>();
+    public static List<Map<String, String>> table1Source = new ArrayList<>();
+
+    static {
+        values.put("S2_R1_FName", "John");
+        values.put("S2_R1_MI", "Doe");
+        values.put("S2_R1_LName", "Smith");
+        values.put("agreement.amount", "1000 $");
+        values.put("agreement.amount.text", "One thousand dollars");
+        values.put("AssetStatusName", "One thousand dollars");
+
+
+        Map<String, String> row1 = new HashMap<>();
+        row1.put("document.number", "1");
+        row1.put("document.name", "Document 1");
+        row1.put("number.of.pages", "234");
+
+        Map<String, String> row2 = new HashMap<>();
+        row2.put("document.number", "2");
+        row2.put("document.name", "Document 2");
+        row2.put("number.of.pages", "152");
+
+        Map<String, String> row3 = new HashMap<>();
+        row3.put("document.number", "3");
+        row3.put("document.name", "Document 3");
+        row3.put("number.of.pages", "15221");
+
+        table1Source.add(row1);
+        table1Source.add(row2);
+        table1Source.add(row3);
     }
 }
