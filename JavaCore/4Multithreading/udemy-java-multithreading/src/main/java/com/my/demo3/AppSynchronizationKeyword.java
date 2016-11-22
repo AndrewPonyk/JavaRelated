b5526d79cdf65f2a7f8faa9532982c7255e52601
Andrew <andrew9999@ukr.net>
2016-11-22 09:32:42 +0200

    Some changes to Udemy multith project

---
+++ JavaCore/4Multithreading/udemy-java-multithreading/src/main/java/com/my/demo3/AppSynchronizationKeyword.java
@@ -0,0 +1,13 @@
+package com.my.demo3;
+
+/**
+ * Created by andrii on 22.11.16.
+ */
+public class AppSynchronizationKeyword {
+
+    private int count = 0;
+
+    public static void main(String[] args) {
+        System.out.println("Incrementing");
+    }
+}
---
+++ JavaCore/4Multithreading/udemy-java-multithreading/src/test/java/com/my/temp.java
@@ -0,0 +1,7 @@
+package com.my;
+
+/**
+ * Created by andrii on 22.11.16.
+ */
+public class temp {
+}
