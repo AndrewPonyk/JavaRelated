23cf330d638ba47a8e99a882ebfe08d3c0372d20
Andrew <andrew9999@ukr.net>
2016-11-29 08:23:59 +0200

    Changes in RMI project

--- JavaCore/RMI_and_JMX/RMI/hello-rmi/src/main/java/com/ap/WeatherServiceImpl.java
+++ JavaCore/RMI_and_JMX/RMI/hello-rmi/src/main/java/com/ap/WeatherServiceImpl.java
@@ -1,10 +1,14 @@
 package com.ap;
 
 import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.Date;
 
 
-public class WeatherServiceImpl implements WeatherService{
+public class WeatherServiceImpl extends UnicastRemoteObject implements WeatherService{
+
+    protected WeatherServiceImpl() throws RemoteException {
+    }
 
     public WeatherData getWeather(Date date, String location) throws RemoteException {
         return new WeatherData(new Date(), Constants.LOCATION_TOKYO, Constants.WEATHER_RAIN);
