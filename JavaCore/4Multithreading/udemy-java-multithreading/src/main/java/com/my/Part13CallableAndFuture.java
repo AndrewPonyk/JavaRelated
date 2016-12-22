566fdfe35b09000c77e68a736650031e930d3d9a
Andrii Ponyk <APonyk@agileassets.com>
2016-12-22 10:50:40 +0200

    Java Multithred : added examples 'Future&Callable' and Low level producer consumer

--- JavaCore/4Multithreading/udemy-java-multithreading/src/main/java/com/my/Part13CallableAndFuture.java
+++ JavaCore/4Multithreading/udemy-java-multithreading/src/main/java/com/my/Part13CallableAndFuture.java
@@ -1,10 +1,54 @@
 package com.my;
 
+import java.util.concurrent.*;
+
 /**
  * Created by andrii on 27.11.16.
  */
 public class Part13CallableAndFuture {
 	public static void main(String[] args) {
 		System.out.println("Java8 eclipse");
+		ExecutorService executor = Executors.newCachedThreadPool();
+
+		Future<Integer> future = executor.submit(new Callable<Integer>() {
+			@Override
+			public Integer call() throws Exception {
+				System.out.println("Starting...");
+				Thread.sleep(700);
+				System.out.println("Finished.");
+				return 192;
+
+			}
+		});
+
+		Future<Integer> secondFuture = executor.submit(new Callable<Integer>() {
+			@Override
+			public Integer call() throws Exception {
+				System.out.println("Starting first callable...");
+				Thread.sleep(1000);
+				System.out.println("Finished.");
+				return 211;
+			}
+		});
+		System.out.println("End of application");
+		for (int i = 0; i < 10; i++) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(i);
+		}
+		try {
+			System.out.println("Result is: " + future.get());
+			System.out.println("Result is: " + secondFuture.get());
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+
+
+		executor.shutdown();
 	} 
 }
--- JavaCore/4Multithreading/udemy-java-multithreading/src/main/java/com/my/Part9ConsumerProducerUsingLowLevel.java
+++ JavaCore/4Multithreading/udemy-java-multithreading/src/main/java/com/my/Part9ConsumerProducerUsingLowLevel.java
@@ -1,7 +1,71 @@
 package com.my;
 
+import java.util.LinkedList;
+import java.util.Random;
+
 /**
  * Created by andrii on 27.11.16.
  */
 public class Part9ConsumerProducerUsingLowLevel {
+    private LinkedList<Integer> list = new LinkedList<>();
+    private final int LIMIT = 10;
+    private Object lock = new Object();
+
+    public void produce() throws InterruptedException {
+        int value = 0;
+        while (true){
+            synchronized (lock){
+                while (list.size() == LIMIT){
+                    lock.wait();
+                }
+                System.out.println("Adding to list value: " + value);
+                list.add(value++);
+
+                lock.notify();
+            }
+            Random r = new Random();
+            Thread.sleep(r.nextInt(100));
+        }
+    }
+
+    public void consume() throws InterruptedException {
+        Random r = new Random();
+        while (true){
+            synchronized (lock){
+                while (list.size()==0){
+                    lock.wait();
+                }
+
+                System.out.print("List size is " + list.size());
+                int value = list.removeFirst();
+                System.out.println("; value is :" + value);
+                lock.notify();
+            }
+
+            Thread.sleep(r.nextInt(1000));
+        }
+    }
+
+    public static void main(String[] args) {
+        Part9ConsumerProducerUsingLowLevel part9ConsumerProducerUsingLowLevel = new Part9ConsumerProducerUsingLowLevel();
+        Thread t1 = new Thread(()->{
+            try {
+                part9ConsumerProducerUsingLowLevel.produce();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread t2 = new Thread(()->{
+            try {
+                part9ConsumerProducerUsingLowLevel.consume();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+    }
 }
