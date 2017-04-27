c4a1211162a33382297d1f10235be0280dfa17c4
AndrewPonyk <andrew9999@ukr.net>
2017-04-27 09:12:51 +0300

    Visualpairprogr added ch12 Java Library:handling dates

--- JavaTempProjects/VirtualPairProgrammers-Courses/1JavaFundamentals/java-fundamentals/src/main/java/ch12JavaLibrary/Main.java
+++ JavaTempProjects/VirtualPairProgrammers-Courses/1JavaFundamentals/java-fundamentals/src/main/java/ch12JavaLibrary/Main.java
@@ -1,12 +1,29 @@
 package ch12JavaLibrary;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 /**
- * Created by andrii on 24.04.17.
+ * Created by andrii on 27.04.17.
  */
 public class Main {
-    public static void main(String[] args) {
 
-        
+    public static void main(String[] args) {
+        // use Java 7 dates and calendar
+
+        Date today = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("MMM -- d -- Y ");
+        System.out.println(sdf.format(today)); // Apr -- 27 -- 2017
+        //https://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html#month
+
+        //https://docs.oracle.com/javase/7/docs/api/java/util/GregorianCalendar.html
+        GregorianCalendar gCal = new GregorianCalendar(2017, 03, 27); // apr 27 2017
+        System.out.println(gCal.getTime());
+        gCal.add(Calendar.WEEK_OF_MONTH, 2);
+        System.out.println(gCal.getTime()); // 11 may
+
 
     }
 }
---
+++ JavaTempProjects/VirtualPairProgrammers-Courses/1JavaFundamentals/java-fundamentals/src/main/java/ch13HandlingNumbers/Main.java
@@ -0,0 +1,10 @@
+package ch13HandlingNumbers;
+
+/**
+ * Created by andrii on 27.04.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
