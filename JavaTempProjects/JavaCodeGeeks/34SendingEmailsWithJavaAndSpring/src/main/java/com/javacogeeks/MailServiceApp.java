8c05a4a4c08648bb4175d1bfef0697840cc90f56
Andrii Ponyk <APonyk@agileassets.com>
2017-05-03 21:41:57 +0300

    Javacodegeeks Spring mail sending init

---
+++ JavaTempProjects/JavaCodeGeeks/34SendingEmailsWithJavaAndSpring/src/main/java/com/javacogeeks/MailServiceApp.java
@@ -0,0 +1,13 @@
+package com.javacogeeks;
+
+/**
+ * Hello world!
+ *
+ */
+public class MailServiceApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
