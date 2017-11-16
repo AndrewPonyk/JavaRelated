e86a7f026453e062b0f313c48f6e3aa89a8d1ce9
ponykand <Andrii.Ponyk@externals.adidas.com>
2017-11-16 20:31:49 +0200

    spring cloud lynda - in progress

--- JavaEE/5SPRING/Lynda-SpringCloud/spring-cloud/eureka-server/src/main/java/com/ap/EurekaServerApplication.java
+++ JavaEE/5SPRING/Lynda-SpringCloud/spring-cloud/eureka-server/src/main/java/com/ap/EurekaServerApplication.java
@@ -2,13 +2,13 @@ package com.ap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableConfigServer
-public class ConfigServerApplication {
+@EnableEurekaServer
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 }
