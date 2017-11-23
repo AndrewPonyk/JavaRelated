88252198804bb9dc5d63d0fc98509dbdab01a8be
ponykand <Andrii.Ponyk@externals.adidas.com>
2017-11-23 17:45:15 +0200

    update java mail project: send html table

--- JavaTempProjects/JavaCodeGeeks/34SendingEmailsWithJavaAndSpring/src/main/java/com/JavaCoreSendMailApp.java
+++ JavaTempProjects/JavaCodeGeeks/34SendingEmailsWithJavaAndSpring/src/main/java/com/JavaCoreSendMailApp.java
@@ -4,10 +4,9 @@ import javax.activation.DataHandler;
 import javax.activation.DataSource;
 import javax.activation.FileDataSource;
 import javax.mail.*;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
+import javax.mail.internet.*;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
 import java.util.Properties;
 
@@ -16,7 +15,7 @@ import java.util.Properties;
 public class JavaCoreSendMailApp {
 
     public static void main(String[] args) throws MessagingException {
-        sendTextAndHtmlText("", "bet notification", "1.5:"+new Date(),
+        sendHtmlTable("", "bet notification", Arrays.asList("1.5000000000000000000000:" + new Date(), "second row"),
                 "", "");
         //sendEmailWithAttachment("", "bet notification", "sample text", "", "");
 
@@ -25,15 +24,6 @@ public class JavaCoreSendMailApp {
     private static void sendEmailWithAttachment(String to, String subject, String msg, String from, String password) throws MessagingException {
         // worked example from here (7 votes)
         //http://stackoverflow.com/questions/10509699/must-issue-a-starttls-command-first
-
-
-        //7 Votes
-        //smtp port and socketFactory has to be change
-
-//        String to = "andrew9999@ukr.net";
-//        String subject = "Email with hotel image";
-//        final String from = FROM;
-//        final String password = PASSWORD;
 
         Session session = getSession(from, password);
 
@@ -65,19 +55,26 @@ public class JavaCoreSendMailApp {
         transport.close();
     }
 
-    private static void sendTextAndHtmlText(String to, String subject, String msg, String from, String password) throws MessagingException {
+    private static void sendTextMessage(String to, String subject, String msg, String from, String password) throws MessagingException {
+        Session session = getSession(from, password);
+
+        // can be added "session.setDebug(true);"
+        Transport transport = session.getTransport();
+        InternetAddress addressFrom = new InternetAddress(from);
+        MimeMessage message = new MimeMessage(session);
+        message.setSender(addressFrom);
+        message.setSubject(subject);
+        message.setContent(msg, "text/plain");
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+        transport.connect();
+        Transport.send(message);
+        transport.close();
+    }
+
+    private static void sendHtmlTable(String to, String subject, Collection<String> rowsText, String from, String password) throws MessagingException {
         // worked example from here (7 votes)
         //http://stackoverflow.com/questions/10509699/must-issue-a-starttls-command-first
-
-
-        //7 Votes
-        //smtp port and socketFactory has to be change
-
-//        String to = "andrew9999@ukr.net";
-//        String subject = "subject";
-//        String msg = "email text....";
-//        final String from = FROM;
-//        final String password = PASSWORD;
 
 
         Session session = getSession(from, password);
@@ -86,25 +83,28 @@ public class JavaCoreSendMailApp {
         Transport transport = session.getTransport();
         InternetAddress addressFrom = new InternetAddress(from);
 
-        MimeMessage message = new MimeMessage(session);
-        message.setSender(addressFrom);
-        message.setSubject(subject);
-        message.setContent(msg, "text/plain");
-        message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
 
         MimeMessage messageWithHtml = new MimeMessage(session);
+        messageWithHtml.setSubject(subject);
         Multipart mp = new MimeMultipart();
         MimeBodyPart htmlPart = new MimeBodyPart();
-        htmlPart.setContent("<b>bold</b>text", "text/html");
+
+        StringBuilder tableHtml = new StringBuilder("<table style='border-collapse:collapse; border: 2px solid grey'>");
+        rowsText.stream().forEach(e -> tableHtml.append(getTableRow(e)));
+        tableHtml.append("</table>");
+        htmlPart.setContent(tableHtml.toString(), "text/html");
+
         mp.addBodyPart(htmlPart);
         messageWithHtml.setContent(mp);
         messageWithHtml.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
 
 
-        transport.connect();
-        Transport.send(message);
-        //Transport.send(messageWithHtml);
+        Transport.send(messageWithHtml);
         transport.close();
+    }
+
+    private static String getTableRow(String text) {
+        return "<tr><td style='border-collapse: collapse;border: 1px solid grey;padding:5px'>" + text + "</td></tr>";
     }
 
     private static Session getSession(String from, String password) {
